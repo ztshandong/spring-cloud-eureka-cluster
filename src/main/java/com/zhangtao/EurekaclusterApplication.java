6959766f7ec3a07bd02fd78ca48d405468f65bea
zhangtao <ztshandong@gmail.com>
2017-06-16 22:58:53 +0800

    spring cloud eureka cluster

---
+++ src/main/java/com/zhangtao/EurekaclusterApplication.java
@@ -0,0 +1,22 @@
+package com.zhangtao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaclusterApplication extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclusterApplication.class, args);
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+	}
+}
