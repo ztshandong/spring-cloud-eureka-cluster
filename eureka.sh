7bacfe169a3fdc4c431ec089b6e1861dedec4c36
zhangtao <ztshandong@gmail.com>
2017-06-28 08:51:20 +0800

    better

---
+++ eureka.sh
@@ -0,0 +1,3 @@
+nohup java -jar target/eurekacluster-0.0.1-SNAPSHOT.jar --spring.profiles.active=first&
+nohup java -jar target/eurekacluster-0.0.1-SNAPSHOT.jar --spring.profiles.active=second&
+nohup java -jar target/eurekacluster-0.0.1-SNAPSHOT.jar --spring.profiles.active=third&
